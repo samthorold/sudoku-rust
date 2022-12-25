3240334567c2c775974a296c8215168c35a467d4
Sam Thorold <sam.thorold@gmail.com>
2022-12-25 14:28:36 +0000

    Ingest board string

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-#[derive(Copy, Clone, Debug)]
+use std::env;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd)]
 struct Cell {
-    col: u8,
     row: u8,
+    col: u8,
     sqr: u8,
     value: u8,
+    poss_idx: usize,
 }
 
 impl Cell {
-    fn with_value(self, value: u8) -> Cell {
-        return Cell { value, ..self };
+    fn set_value(self, value: u8) -> Cell {
+        return Cell {
+            value,
+            poss_idx: self.poss_idx + 1,
+            ..self
+        };
     }
 }
 
 #[derive(Copy, Clone)]
 struct Board {
     cells: [Cell; 81],
+    generation: u32,
+    idx: usize,
+    direction: i8,
 }
 
 impl Board {
@@ -35,9 +45,7 @@ impl Board {
         }
         return s;
     }
-}
 
-impl Board {
     fn neighbours(self, cell: Cell) -> Vec<Cell> {
         let mut nghs = Vec::new();
         for friend in self.cells {
@@ -50,25 +58,87 @@ impl Board {
         }
         return nghs;
     }
+
+    fn possibilities(self, cell: Cell) -> Vec<u8> {
+        let mut poss: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for neighbour in self.neighbours(cell) {
+            if neighbour.value > 0 {
+                let idx = match poss.binary_search(&neighbour.value) {
+                    Ok(idx) => idx,
+                    Err(_) => continue,
+                };
+                poss.remove(idx);
+            }
+        }
+        return poss;
+    }
+
+    fn next_generation(self) -> Board {
+        let idx = self.idx;
+        let cell: Cell = self.cells[self.idx];
+        println!("{cell:#?}");
+        if cell.value > 0 {
+            let idx_increment = self.idx + 1 * usize::try_from(self.direction).unwrap();
+            println!("idx_increment {idx_increment}");
+            return Board {
+                cells: self.cells,
+                generation: self.generation,
+                idx: self.idx + idx_increment,
+                direction: self.direction,
+            };
+        }
+        let poss: Vec<u8> = self.possibilities(cell);
+        if poss.is_empty() {
+            return Board {
+                cells: self.cells,
+                generation: self.generation + 1,
+                idx: self.idx - 1,
+                direction: -1,
+            };
+        } else {
+            let this_poss: u8 = poss[cell.poss_idx];
+            let new_cell: Cell = cell.set_value(this_poss);
+            let mut new_cells: [Cell; 81] = self.cells;
+            new_cells[self.idx] = new_cell;
+            return Board {
+                cells: new_cells,
+                generation: self.generation + 1,
+                idx: self.idx + 1,
+                direction: 1,
+            };
+        }
+    }
 }
 
-fn new_board() -> Board {
+fn new_board(board_string: &String) -> Board {
+    let mut idx: usize = 0;
+    let digits: Vec<u8> = board_string
+        .chars()
+        .map(|s| s.to_digit(10).expect("parse error") as u8)
+        .collect();
     let mut cells: Vec<Cell> = Vec::new();
     for row in 1..10 {
         for col in 1..10 {
             cells.push(Cell {
-                value: 0,
+                value: digits[idx],
                 row,
                 col,
                 sqr: sqr_idx(col, row),
-            })
+                poss_idx: 0,
+            });
+            idx += 1;
         }
     }
     let cells_array = match <[Cell; 81]>::try_from(cells) {
         Ok(arr) => arr,
         Err(_) => panic!("Could not convert cells to array."),
     };
-    return Board { cells: cells_array };
+    return Board {
+        cells: cells_array,
+        generation: 0,
+        idx: 0,
+        direction: 1,
+    };
 }
 
 fn sqr_idx(col: u8, row: u8) -> u8 {
@@ -77,16 +147,34 @@ fn sqr_idx(col: u8, row: u8) -> u8 {
 
 fn main() {
     println!("Sudoku (Rust)");
-    let board = new_board();
+    let args: Vec<String> = env::args().collect();
+    let board_string = &args[1];
+    // dbg!(board_string);
+
+    let mut board = new_board(board_string);
     let board_string = board.string();
+    let generation = board.generation;
+    let idx = board.idx;
+    println!("generation {generation}");
+    println!("idx {idx}");
     println!("{board_string}");
-    let nghs = board.neighbours(Cell {
-        value: 0,
-        col: 1,
-        row: 1,
-        sqr: 1,
-    });
-    println!("{nghs:#?}");
+    for _ in 0..3 {
+        board = board.next_generation();
+        let board_string = board.string();
+        let generation = board.generation;
+        let idx = board.idx;
+        println!("generation {generation}");
+        println!("idx {idx}");
+        println!("{board_string}");
+    }
+    // let nghs = board.neighbours(Cell {
+    //     value: 0,
+    //     col: 1,
+    //     row: 1,
+    //     sqr: 1,
+    //     poss_idx: 0,
+    // });
+    // println!("{nghs:#?}");
 }
 
 #[cfg(test)]
@@ -103,9 +191,10 @@ mod tests {
             col,
             row,
             sqr,
+            poss_idx: 0,
         };
 
-        let new_cell = cell.with_value(1);
+        let new_cell = cell.set_value(1);
         assert_eq!(1, new_cell.value);
         assert_eq!(1, new_cell.col);
         assert_eq!(1, new_cell.row);
@@ -114,7 +203,7 @@ mod tests {
 
     #[test]
     fn test_new_board() {
-        let board = new_board();
+        let _ = new_board();
     }
 
     #[test]
