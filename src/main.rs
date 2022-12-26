056b02fa912a4fa0d31976a779fdfa8a628d3706
Sam Thorold <sam.thorold@gmail.com>
2022-12-26 18:46:41 +0000

    Formatting and solve test

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,7 @@ impl Cell {
                 };
             }
         }
-        return Cell {
-            value: 0,
-            ..self
-        }
+        return Cell { value: 0, ..self };
     }
 }
 
@@ -109,17 +106,20 @@ impl Board {
     fn next_generation(self) -> Board {
         let cell: Cell = self.cells[self.idx];
         if cell.og {
-            let idx = usize::try_from(
-                max(
-                    <i32>::try_from(self.idx).unwrap() + 1 * self.direction,
-                    0
-                )
-            ).unwrap();
+            let idx = usize::try_from(max(
+                <i32>::try_from(self.idx).unwrap() + 1 * self.direction,
+                0,
+            ))
+            .unwrap();
             let direction = match idx {
                 0 => 1,
-                _ => self.direction
+                _ => self.direction,
             };
-            return Board { idx, direction, ..self };
+            return Board {
+                idx,
+                direction,
+                ..self
+            };
         }
         let poss: Vec<u8> = self.possibilities(cell);
         let mut new_cell: Cell = cell.next_value(poss);
@@ -132,7 +132,9 @@ impl Board {
                 direction: 1,
             };
         } else {
-            new_cell.tried = [false, false, false, false, false, false, false, false, false];
+            new_cell.tried = [
+                false, false, false, false, false, false, false, false, false,
+            ];
             new_cells[self.idx] = new_cell;
             let idx = max(self.idx - 1, 0);
             let direction = match idx {
@@ -151,56 +153,58 @@ impl Board {
     fn is_completed(self) -> bool {
         return self.idx > 80;
     }
-}
 
-fn new_board(board_string: &String) -> Board {
-    let mut idx: usize = 0;
-    let digits: Vec<u8> = board_string
-        .chars()
-        .map(|s| s.to_digit(10).expect("parse error") as u8)
-        .collect();
-    let mut cells: Vec<Cell> = Vec::new();
-    for row in 1..10 {
-        for col in 1..10 {
-            let value = digits[idx];
-            let og = match value {
-                0 => false,
-                _ => true,
-            };
-            cells.push(Cell {
-                value,
-                row,
-                col,
-                sqr: sqr_idx(col, row),
-                tried: [false, false, false, false, false, false, false, false, false],
-                og,
-            });
-            idx += 1;
+    fn new(board_string: &String) -> Board {
+        let mut idx: usize = 0;
+        let digits: Vec<u8> = board_string
+            .chars()
+            .map(|s| s.to_digit(10).expect("parse error") as u8)
+            .collect();
+        let mut cells: Vec<Cell> = Vec::new();
+        for row in 1..10 {
+            for col in 1..10 {
+                let value = digits[idx];
+                let og = match value {
+                    0 => false,
+                    _ => true,
+                };
+                cells.push(Cell {
+                    value,
+                    row,
+                    col,
+                    sqr: sqr_idx(col, row),
+                    tried: [
+                        false, false, false, false, false, false, false, false, false,
+                    ],
+                    og,
+                });
+                idx += 1;
+            }
         }
+        let cells_array = match <[Cell; 81]>::try_from(cells) {
+            Ok(arr) => arr,
+            Err(_) => panic!("Could not convert cells to array."),
+        };
+        return Board {
+            cells: cells_array,
+            idx: 0,
+            direction: 1,
+        };
     }
-    let cells_array = match <[Cell; 81]>::try_from(cells) {
-        Ok(arr) => arr,
-        Err(_) => panic!("Could not convert cells to array."),
-    };
-    return Board {
-        cells: cells_array,
-        idx: 0,
-        direction: 1,
-    };
 }
-
 fn sqr_idx(col: u8, row: u8) -> u8 {
     return (col - 1) / 3 + 3 * ((row - 1) / 3) + 1;
 }
 
-fn solve(mut board: Board) -> Board {
+fn solve(board: Board) -> Board {
+    let mut b = board;
     for _ in 0..1000000 {
-        if board.is_completed() {
-            break
+        if b.is_completed() {
+            break;
         }
-        board = board.next_generation();
+        b = b.next_generation();
     }
-    return board
+    return b;
 }
 
 fn main() {
@@ -208,7 +212,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let board_string = &args[1];
 
-    let board = new_board(board_string);
+    let board = Board::new(board_string);
     let board_string = board.string();
     println!("{board_string}");
 
@@ -223,50 +227,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cell_with_value() {
-        let col: u8 = 1;
-        let row: u8 = 1;
-        let sqr: u8 = 1;
-        let cell = Cell {
-            value: 0,
-            col,
-            row,
-            sqr,
-            poss_idx: 0,
-            og: false,
-        };
-
-        let new_cell = cell.set_value(1);
-        assert_eq!(1, new_cell.value);
-        assert_eq!(1, new_cell.col);
-        assert_eq!(1, new_cell.row);
-        assert_eq!(1, new_cell.sqr);
-    }
-
-    #[test]
     fn test_new_board() {
+        let exp = "\n\
+        534|678|912\n\
+        672|195|348\n\
+        198|342|567\n\
+        ---+---+---\n\
+        859|761|423\n\
+        426|853|791\n\
+        713|924|856\n\
+        ---+---+---\n\
+        961|537|284\n\
+        287|419|635\n\
+        345|286|179\n\
+        ";
         let board_string = String::from(
             "530070000600195000098000060800060003400803001700020006060000280000419005000080079",
         );
-        let _ = new_board(&board_string);
-    }
-
-    #[test]
-    fn test_sqr_idx() {
-        let exp: u8 = 1;
-        let got: u8 = sqr_idx(1, 1);
-        assert_eq!(exp, got);
-
-        let exp: u8 = 9;
-        let got: u8 = sqr_idx(9, 9);
-        assert_eq!(exp, got);
-
-        let exp: u8 = 4;
-        let got: u8 = sqr_idx(1, 4);
-        assert_eq!(exp, got);
-
-        let exp: u8 = 8;
-        let got: u8 = sqr_idx(5, 9);
-        assert_eq!(exp, got);
+        let board = Board::new(&board_string);
+        let solution = solve(board);
+        assert!(solution.is_completed());
+        assert_eq!(solution.string(), exp);
     }
 }
