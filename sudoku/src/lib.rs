b6a2400007cab2adfa7b818b90614bef7609e766
Sam Thorold <sam.thorold@gmail.com>
2023-01-29 17:08:42 +0000

    Cargo t

--- sudoku/src/lib.rs
+++ sudoku/src/lib.rs
@@ -1,6 +1,5 @@
-//! Sudoku solver implementation in Rust.
+//! Sudoku game
 
-use std::cmp::max;
 use std::fmt;
 
 /// Digit box in a Sudoku board.
@@ -9,15 +8,13 @@ pub struct Cell {
     row: u8,
     col: u8,
     sqr: u8,
-    value: u8,
-    tried: [bool; 9],
-    poss: [bool; 9],
+    val: u8,
     og: bool,
 }
 
 impl fmt::Display for Cell {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "c{}r{}v{}", self.col, self.row, self.value)
+        write!(f, "c{}r{}v{}", self.col, self.row, self.val)
     }
 }
 
@@ -31,26 +28,25 @@ impl Cell {
     /// * `value` - value, 0-9 where 0 means unset.
     /// * `row` - row or y-coordinate on the board.
     /// * `col` - col or x-coordinate on the board.
-    /// * `sqr` - 3x3 box the `Cell` sits in.
     /// * `og` - did the `Cell` have a value when the `Board` was created?
     ///
     /// # Examples
     ///
     /// ```
-    /// use sudoku_rust::Cell;
-    /// let cell = Cell::new(1, 1, 1, 1, true);
+    /// use sudoku::Cell;
+    /// let cell = Cell::new(1, 1, 1);
     /// ```
-    pub fn new(value: u8, row: u8, col: u8, sqr: u8, og: bool) -> Cell {
+    pub fn new(val: u8, row: u8, col: u8) -> Cell {
+        let og = match val {
+            0 => false,
+            _ => true,
+        };
         return Cell {
             row,
             col,
-            sqr,
+            sqr: sqr_idx(col, row),
             og,
-            value,
-            tried: [
-                false, false, false, false, false, false, false, false, false,
-            ],
-            poss: [true, true, true, true, true, true, true, true, true],
+            val,
         };
     }
 
@@ -58,69 +54,64 @@ impl Cell {
         return !self.og;
     }
 
-    /// Set the possible values for the Cell given its neighbours.
-    ///
-    /// # Arguments
-    ///
-    /// * neighbours - Neighbour cells from the same row, column, and square.
-    pub fn set_possibilities(&mut self, neighbours: &Vec<Cell>) {
-        for neighbour in neighbours {
-            if neighbour.is_set() {
-                self.poss[usize::try_from(neighbour.value - 1).unwrap()] = false;
-            }
+    pub fn set(&mut self, val: u8) {
+        if !self.can_set() {
+            panic!("Cannot set an OG Cell");
         }
-    }
-
-    /// Update Cell value with the next untried value from the possibilities.
-    pub fn set_value(&mut self) {
-        for (idx, p) in self.poss.iter().enumerate() {
-            if (!self.tried[idx]) && *p {
-                self.tried[idx] = true;
-                self.value = u8::try_from(idx + 1).unwrap();
-                return;
-            }
-        }
-        self.value = 0;
-        return;
+        self.val = val;
     }
 
     pub fn is_set(&self) -> bool {
-        return self.value > 0;
+        return self.val > 0;
     }
 
-    /// Reset the Cell - nothing tried and all values possible.
-    pub fn reset(&mut self) {
-        if self.og {
+    pub fn unset(&mut self) {
+        if !self.can_set() {
             panic!("Cannot reset an OG Cell");
         }
-        self.tried = [
-            false, false, false, false, false, false, false, false, false,
-        ];
-        self.poss = [true, true, true, true, true, true, true, true, true];
-        self.value = 0;
+        self.val = 0;
     }
 }
 
 /// 9x9 Sudoku board.
 pub struct Board {
     cells: [Cell; 81],
-    idx: usize,
-    direction: i32,
 }
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.direction, self.idx)
+        write!(f, "{}", self.to_string())
     }
 }
 
 impl Board {
+    pub fn new(board_string: &String) -> Board {
+        let mut idx: usize = 0;
+        let digits: Vec<u8> = board_string
+            .chars()
+            .map(|s| s.to_digit(10).expect("parse error") as u8)
+            .collect();
+        let mut cells: Vec<Cell> = Vec::new();
+        for row in 1..10 {
+            for col in 1..10 {
+                let val = digits[idx];
+                cells.push(Cell::new(val, row, col));
+                idx += 1;
+            }
+        }
+        let cells_array = match <[Cell; 81]>::try_from(cells) {
+            Ok(arr) => arr,
+            Err(_) => panic!("Could not convert cells to array."),
+        };
+        return Board { cells: cells_array };
+    }
+
     /// String representation of a Board.
     pub fn string(&self) -> String {
         let important_idx: [u8; 2] = [3, 6];
         let mut s: String = String::from("\n");
         for cell in self.cells {
-            s.push_str(&cell.value.to_string());
+            s.push_str(&cell.val.to_string());
             if important_idx.contains(&cell.col) {
                 s.push_str("|")
             }
@@ -151,92 +142,10 @@ impl Board {
         }
         return nghs;
     }
-
-    /// Update the value for a single Cell.
-    // Really, this is part of the backtrack algorithm
-    fn next_generation(&mut self) {
-        let mut cell = self.cells[self.idx];
-        if !cell.can_set() {
-            let idx = usize::try_from(max(
-                <i32>::try_from(self.idx).unwrap() + 1 * self.direction,
-                0,
-            ))
-            .unwrap();
-            let direction = match idx {
-                0 => 1,
-                _ => self.direction,
-            };
-            self.idx = idx;
-            self.direction = direction;
-            return;
-        }
-        let neighbours = self.neighbours(&cell);
-        cell.set_possibilities(&neighbours);
-        cell.set_value();
-        if cell.is_set() {
-            self.cells[self.idx] = cell;
-            self.idx += 1;
-            self.direction = 1;
-        } else {
-            cell.reset();
-            self.cells[self.idx] = cell;
-            let idx = max(self.idx - 1, 0);
-            let direction = match idx {
-                0 => 1,
-                _ => -1,
-            };
-            self.idx = idx;
-            self.direction = direction;
-        }
-    }
-
-    /// Is the Board completed?
-    // Really, this is part of the backtrack algorithm
-    fn is_completed(&self) -> bool {
-        return self.idx > 80;
-    }
-
-    pub fn new(board_string: &String) -> Board {
-        let mut idx: usize = 0;
-        let digits: Vec<u8> = board_string
-            .chars()
-            .map(|s| s.to_digit(10).expect("parse error") as u8)
-            .collect();
-        let mut cells: Vec<Cell> = Vec::new();
-        for row in 1..10 {
-            for col in 1..10 {
-                let value = digits[idx];
-                let og = match value {
-                    0 => false,
-                    _ => true,
-                };
-                cells.push(Cell::new(value, row, col, sqr_idx(col, row), og));
-                idx += 1;
-            }
-        }
-        let cells_array = match <[Cell; 81]>::try_from(cells) {
-            Ok(arr) => arr,
-            Err(_) => panic!("Could not convert cells to array."),
-        };
-        return Board {
-            cells: cells_array,
-            idx: 0,
-            direction: 1,
-        };
-    }
 }
+
 fn sqr_idx(col: u8, row: u8) -> u8 {
     return (col - 1) / 3 + 3 * ((row - 1) / 3) + 1;
-}
-
-pub fn backtrack(board: &mut Board) {
-    for _ in 0..10000000 {
-        if board.is_completed() {
-            return;
-        }
-        board.next_generation();
-    }
-    panic!("Did not find solution.");
 }
 
 #[cfg(test)]
@@ -244,19 +153,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_backtrack() {
+    fn test_new_board() {
         let exp = "\n\
-        534|678|912\n\
-        672|195|348\n\
-        198|342|567\n\
+        530|070|000\n\
+        600|195|000\n\
+        098|000|060\n\
         ---+---+---\n\
-        859|761|423\n\
-        426|853|791\n\
-        713|924|856\n\
+        800|060|003\n\
+        400|803|001\n\
+        700|020|006\n\
         ---+---+---\n\
-        961|537|284\n\
-        287|419|635\n\
-        345|286|179\n\
+        060|000|280\n\
+        000|419|005\n\
+        000|080|079\n\
         ";
         let board_string = String::from(
             "\
@@ -271,9 +180,7 @@ mod tests {
             000080079\
             ",
         );
-        let mut board = Board::new(&board_string);
-        backtrack(&mut board);
-        assert!(board.is_completed());
+        let board = Board::new(&board_string);
         assert_eq!(board.string(), exp);
     }
 }
--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,17 +1,17 @@
 use std::env;
 
-use sudoku_rust;
+use sudoku;
 
 fn main() {
     println!("Sudoku (Rust)");
     let args: Vec<String> = env::args().collect();
     let board_string = &args[1];
 
-    let mut board = sudoku_rust::Board::new(board_string);
+    let board = sudoku::Board::new(board_string);
     let board_string = board.string();
     println!("{board_string}");
 
-    sudoku_rust::backtrack(&mut board);
+    //sudoku::backtrack(&mut board);
 
     let board_string = board.string();
     println!("{board_string}");
