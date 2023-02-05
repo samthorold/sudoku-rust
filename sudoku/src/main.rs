e024f419f6b8f2c4339dc9a773c3ff6ac387bb10
Sam Thorold <sam.thorold@gmail.com>
2023-02-05 22:37:31 +0000

    Refactor cli

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,19 +1,19 @@
 use std::env;
+use std::process;
 
-use backtrack;
-use board;
+use sudoku;
 
 fn main() {
     println!("Sudoku (Rust)");
     let args: Vec<String> = env::args().collect();
-    let board_string = &args[1];
 
-    let mut board = board::Board::new(board_string);
-    let board_string = board.string();
-    println!("{board_string}");
+    let config = sudoku::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args {err}");
+        process::exit(1);
+    });
 
-    backtrack::backtrack(&mut board, &board::Addr { row: 1, col: 1 }, 0);
-
-    let board_string = board.string();
-    println!("{board_string}");
+    if let Err(e) = sudoku::run(config) {
+        println!("App err: {e}");
+        process::exit(1);
+    }
 }
