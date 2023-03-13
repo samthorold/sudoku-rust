17a01268d2cc5305132ce2a9893990894301f305
Sam Thorold <sam.thorold@gmail.com>
2023-03-13 00:52:25 +0000

    DLX cover init

--- dlx/src/lib.rs
+++ dlx/src/lib.rs
@@ -6,11 +6,14 @@ pub struct A {
 
 impl A {
     fn get_node(&self, addr: NodeAddr) -> Node {
+        if (addr.r < 0) | (addr.c < 0) {
+            panic!("Invalid node addr {}{}", addr.r, addr.c);
+        }
         self.nodes[addr.r as usize][addr.c as usize]
     }
 
     fn set_node(&mut self, node: Node) {
-        self.nodes[node.a.r as usize][node.a.c as usize] = node;
+        self.nodes[node.addr.r as usize][node.addr.c as usize] = node;
     }
 
     fn get_col(&self, addr: ColAddr) -> Col {
@@ -21,7 +24,7 @@ impl A {
     }
 
     fn set_col(&mut self, col: Col) {
-        self.cols[col.a.c as usize] = col;
+        self.cols[col.addr.c as usize] = col;
     }
 }
 
@@ -39,7 +42,7 @@ impl ColAddr {
 #[derive(Copy, Clone, Debug)]
 pub struct Col {
     root: bool,
-    a: ColAddr,
+    addr: ColAddr,
     l: ColAddr,
     r: ColAddr,
     d: NodeAddr,
@@ -50,7 +53,7 @@ impl Col {
     fn new(c: i32) -> Col {
         Col {
             root: false,
-            a: ColAddr { c },
+            addr: ColAddr { c },
             l: ColAddr::new(),
             r: ColAddr::new(),
             d: NodeAddr::new(),
@@ -72,25 +75,16 @@ impl Col {
         }
     }
 
-    fn set_l(self, c: i32) -> Col {
-        Col {
-            l: ColAddr { c },
-            ..self
-        }
+    fn set_l(self, l: Col) -> Col {
+        Col { l: l.addr, ..self }
     }
 
-    fn set_r(self, c: i32) -> Col {
-        Col {
-            r: ColAddr { c },
-            ..self
-        }
+    fn set_r(self, r: Col) -> Col {
+        Col { r: r.addr, ..self }
     }
 
-    fn set_d(self, r: i32) -> Col {
-        Col {
-            d: NodeAddr { r, ..self.d },
-            ..self
-        }
+    fn set_d(self, d: Node) -> Col {
+        Col { d: d.addr, ..self }
     }
 }
 
@@ -108,7 +102,8 @@ impl NodeAddr {
 
 #[derive(Copy, Clone, Debug)]
 struct Node {
-    a: NodeAddr,
+    addr: NodeAddr,
+    is_legit: bool,
     c: ColAddr,
     l: NodeAddr,
     r: NodeAddr,
@@ -119,7 +114,8 @@ struct Node {
 impl Node {
     fn new(r: i32, c: i32) -> Node {
         Node {
-            a: NodeAddr { r, c },
+            addr: NodeAddr { r, c },
+            is_legit: false,
             c: ColAddr::new(),
             l: NodeAddr::new(),
             r: NodeAddr::new(),
@@ -128,78 +124,74 @@ impl Node {
         }
     }
 
-    fn set_c(self, c: i32) -> Node {
-        Node {
-            c: ColAddr { c },
-            ..self
-        }
+    fn set_c(self, c: Col) -> Node {
+        Node { c: c.addr, ..self }
     }
 
-    fn set_l(self, c: i32) -> Node {
-        Node {
-            l: NodeAddr { c, ..self.l },
-            ..self
-        }
+    fn set_l(self, l: Node) -> Node {
+        Node { l: l.addr, ..self }
     }
 
-    fn set_r(self, c: i32) -> Node {
-        Node {
-            r: NodeAddr { c, ..self.r },
-            ..self
-        }
+    fn set_r(self, r: Node) -> Node {
+        Node { r: r.addr, ..self }
     }
 
-    fn set_u(self, r: i32) -> Node {
-        Node {
-            u: NodeAddr { r, ..self.u },
-            ..self
-        }
+    fn set_u(self, u: Node) -> Node {
+        Node { u: u.addr, ..self }
     }
 
-    fn set_d(self, r: i32) -> Node {
-        Node {
-            d: NodeAddr { r, ..self.d },
-            ..self
-        }
+    fn set_d(self, d: Node) -> Node {
+        Node { d: d.addr, ..self }
     }
 }
 
+/// Create a problem, `A`, from a matrix of 0s and 1s.
 pub fn from_matrix(matrix: &Vec<Vec<u8>>) -> A {
+    // the "root" col is used to start off each iteration of covering
     let mut root = Col::new(0);
     root.root = true;
+
     let mut cols = <Vec<Col>>::new();
     let mut nodes = <Vec<Vec<Node>>>::new();
+
     for (r, row) in matrix.into_iter().enumerate() {
         let mut row_nodes = <Vec<Node>>::new();
-        let r32 = r.try_into().unwrap();
+
         for (c, val) in row.into_iter().enumerate() {
-            let c32 = c.try_into().unwrap();
             if r == 0 {
-                cols.push(Col::new(c32));
+                cols.push(Col::new(c as i32));
                 if c > 0 {
-                    cols[c] = cols[c].set_l(c32 - 1);
-                    cols[c - 1] = cols[c - 1].set_r(c32);
+                    cols[c] = cols[c].set_l(cols[c - 1]);
+                    cols[c - 1] = cols[c - 1].set_r(*cols.last().unwrap());
                 }
             }
-            row_nodes.push(Node::new(r32, c32));
+            row_nodes.push(Node::new(r as i32, c as i32));
             if *val == 1 {
                 println!("{r}{c}");
+                row_nodes[c].is_legit = true;
+
                 cols[c] = cols[c].incr_s();
-                row_nodes[c] = row_nodes[c].set_c(c32);
+                row_nodes[c] = row_nodes[c].set_c(*cols.last().unwrap());
 
                 if cols[c].d.r < 0 {
-                    cols[c] = cols[c].set_d(r32)
+                    // column's down addr will be initialised to -1, -1
+                    // so down.right -1 means no down addr set yet for a column
+                    cols[c] = cols[c].set_d(row_nodes[c]);
                 } else {
-                    let mut u = cols[c].d.r;
+                    // otherwise we need to find the last legit node
+                    // and connect the last legit node and the current node
+                    let mut u = r as i32;
                     loop {
-                        let next_u = nodes[u as usize][c].d.r;
-                        if next_u < 0 {
-                            row_nodes[c] = row_nodes[c].set_u(u);
-                            nodes[u as usize][c] = nodes[u as usize][c].set_d(r32);
+                        u = u - 1;
+                        if u < 0 {
+                            break;
+                        }
+                        if nodes[u as usize][c].is_legit {
+                            nodes[u as usize][c] =
+                                nodes[u as usize][c].set_d(*row_nodes.last().unwrap());
+                            row_nodes[c] = row_nodes[c].set_u(nodes[u as usize][c]);
                             println!("  u={u}, d={r}");
                             break;
-                        } else {
-                            u = next_u;
                         }
                     }
                 }
@@ -211,9 +203,9 @@ pub fn from_matrix(matrix: &Vec<Vec<u8>>) -> A {
                         if l < 0 {
                             break;
                         }
-                        if row_nodes[l as usize].c.c >= 0 {
-                            row_nodes[c] = row_nodes[c].set_l(l);
-                            row_nodes[l as usize] = row_nodes[l as usize].set_r(c32);
+                        if row_nodes[l as usize].is_legit {
+                            row_nodes[c] = row_nodes[c].set_l(row_nodes[l as usize]);
+                            row_nodes[l as usize] = row_nodes[l as usize].set_r(row_nodes[c]);
                             println!("  l={l}, r={c}");
                             break;
                         }
@@ -221,38 +213,55 @@ pub fn from_matrix(matrix: &Vec<Vec<u8>>) -> A {
                 }
             }
         }
-        let mut l = 0;
-        let mut r = row_nodes.len();
+        let mut l_ = 0;
+        let mut r_ = row_nodes.len();
 
         // get to the rightmost legit node
         loop {
-            r = r - 1;
-            if row_nodes[r].c.c >= 0 {
+            r_ = r_ - 1;
+            if row_nodes[r_].is_legit {
                 break;
             }
         }
 
         // get to the leftmost legit node
         loop {
-            if row_nodes[l].c.c >= 0 {
+            if row_nodes[l_].is_legit {
                 break;
             }
-            l = l + 1;
+            l_ = l_ + 1;
         }
 
-        row_nodes[r] = row_nodes[r].set_r(l as i32);
-        row_nodes[l] = row_nodes[l].set_l(r as i32);
+        println!("Setting r={r} lr ...");
+        println!("  l={l_}, r={r_}");
+
+        row_nodes[r_] = row_nodes[r_].set_r(row_nodes[l_]);
+        row_nodes[l_] = row_nodes[l_].set_l(row_nodes[r_]);
 
         nodes.push(row_nodes);
-
-        // TODO: UD for bottom and top nodes
-
-        root = root.set_r(0);
-        root = root.set_l(cols.len() as i32 - 1);
-        cols[0] = cols[0].set_l(-1);
-        let w = cols.len();
-        cols[w - 1] = cols[w - 1].set_r(-1);
     }
+
+    let h = nodes.len();
+    let w = cols.len();
+
+    for j in 0..w {
+        println!("Setting c={j} ud ...");
+        let u = cols[j].d;
+        for i in (0..h).rev() {
+            // println!("{i}");
+            if nodes[i][j].is_legit {
+                nodes[i][j] = nodes[i][j].set_d(nodes[u.r as usize][j]);
+                nodes[u.r as usize][j] = nodes[u.r as usize][j].set_u(nodes[i][j]);
+                println!("  u={i}, d={}", u.r);
+                break;
+            }
+        }
+    }
+
+    root = root.set_r(cols[0]);
+    root = root.set_l(*cols.last().unwrap());
+    cols[0] = cols[0].set_l(root);
+    cols[w - 1] = cols[w - 1].set_r(root);
     return A { root, cols, nodes };
 }
 
@@ -262,34 +271,34 @@ pub fn cover(a: &mut A, c: usize) {
     if col.s == 0 {
         return;
     }
-    // TODO: and if l or r is root?
     let l = a.get_col(col.l);
     let r = a.get_col(col.r);
-    a.set_col(l.set_r(r.a.c));
-    a.set_col(r.set_l(l.a.c));
+    a.set_col(l.set_r(r));
+    a.set_col(r.set_l(l));
 
     let mut cover_node = a.get_node(col.d);
-    if cover_node.a.r < 0 {
-        return;
-    }
+    println!("  cover_node={:#?}", cover_node);
     loop {
         let mut node = a.get_node(cover_node.r);
+        println!("    node={:#?}", node);
         loop {
-            if node.a == cover_node.a {
+            if node.addr == cover_node.addr {
                 break;
             }
-            // TODO: and if u or d is root?
             let u = a.get_node(node.u);
             let d = a.get_node(node.d);
-            a.set_node(u.set_d(d.a.r));
-            a.set_node(d.set_u(u.a.r));
+            a.set_node(u.set_d(d));
+            a.set_node(d.set_u(u));
             a.set_col(col.decr_s());
             node = a.get_node(node.r);
         }
-        if cover_node.d.r < 0 {
+        let maybe_cover_node = cover_node.d;
+        // if we've gone back up the matrix, break
+        if maybe_cover_node.r < cover_node.addr.r {
             break;
         }
-        cover_node = a.get_node(cover_node.d);
+        cover_node = a.get_node(maybe_cover_node);
+        println!("  cover_node={:#?}", cover_node);
     }
 }
 
@@ -306,7 +315,7 @@ mod tests {
             vec![0, 1, 0, 0, 0, 0, 1],
             vec![0, 0, 0, 1, 1, 0, 1],
         ];
-        let a = from_matrix(&matrix);
-        let a = from_matrix(&matrix);
+        let _ = from_matrix(&matrix);
+        let _ = from_matrix(&matrix);
     }
 }
